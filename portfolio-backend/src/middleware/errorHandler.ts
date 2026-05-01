d8dc69a4a4aef65639257570b151350eab4ea216
fiamanillah <fiamanillahb2@gmail.com>
2026-05-02 01:58:33 +0600

    refactor: remove unused components and utilities from the frontend

--- portfolio-backend/src/middleware/errorHandler.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorRequestHandler } from 'express';
-// Error handling middleware
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    res.status(500).json({ message: err.message });
-};
-
-export default errorHandler;
--- portfolio-backend/src/routes/formRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import contactFormController from '../controllers/contactFormController';
-
-const formRoutes = express.Router();
-
-formRoutes.post('/contact', contactFormController);
-
-export default formRoutes;
